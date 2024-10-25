cd3964a0ac9c0fafa67494146ba1d3de099f5ba0
CaptainAweYeah <ashmorewillard@gmail.com>
2024-10-25 11:44:54 -0500

    added client method

---
+++ client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPme
+{
+    public class ClientLink
+    {
+        async internal void Client(string message, IPAddress ipAddress)
+        {
+            IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+            using Socket client = new(
+                ipEndPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            await client.ConnectAsync(ipEndPoint);
+            while (true)
+            {
+                // Send message.
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                _ = await client.SendAsync(messageBytes, SocketFlags.None);
+                Console.WriteLine($"Socket client sent message: \"{message}\"");
+
+                // Receive ack.
+                var buffer = new byte[1_024];
+                var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+                if (response == "<|ACK|>")
+                {
+                    Console.WriteLine(
+                        $"Socket client received acknowledgment: \"{response}\"");
+                    break;
+                }
+            }
+
+            client.Shutdown(SocketShutdown.Both);
+        }
+    }
+}
